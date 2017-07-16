2d0ff7de413b4ac4ffde43bc744dbd2a69c363ba
Brian Ginsburg <brian@brianginsburg.com>
2017-07-16 11:36:52 -0700

    Add try-except in case something goes wrong

--- server/tail.py
+++ server/tail.py
@@ -14,10 +14,13 @@ while True:
         time.sleep(1)
         file.seek(fp)
     else:
-        parsed_data = json.loads(line)
-        response = urllib2.urlopen('https://freegeoip.net/json/%s' % parsed_data['remote_addr'])
-        geoip_data = json.load(response)
-        combined_data = parsed_data.copy()
-        combined_data.update(geoip_data)
-        request = json.dumps(combined_data)
-        os.system("curl -X PUT http://DOMAIN_OR_IP_ADDRESS/nginlog/requests/%s -d '%s'" % (str(uuid.uuid4()), request))
+        try:
+            parsed_data = json.loads(line)
+            response = urllib2.urlopen('https://freegeoip.net/json/%s' % parsed_data['remote_addr'])
+            geoip_data = json.load(response)
+            combined_data = parsed_data.copy()
+            combined_data.update(geoip_data)
+            request = json.dumps(combined_data)
+            os.system("curl -X PUT http://DOMAIN_OR_IP_ADDRESS/nginlog/requests/%s -d '%s'" % (str(uuid.uuid4()), request))
+        except:
+            continue
