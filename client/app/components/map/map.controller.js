5c3347cf3077c9163dd20fd1535ecd2e2b490f71
Brian Ginsburg <brian@brianginsburg.com>
2017-08-06 09:52:54 -0700

    Add bubblemap

--- client/app/components/map/map.controller.js
+++ client/app/components/map/map.controller.js
@@ -5,6 +5,49 @@
     .controller('mapController', mapController);
 
   function mapController($scope) {
+    $scope.mapConfig = {
+      chart: {
+        borderWidth: 1,
+        type: 'map',
+        map: 'custom/world',
+        backgroundColor: 'transparent'
+      },
 
+      title: {
+        text: 'World population 2013 by country'
+      },
+
+      subtitle: {
+        text: 'Demo of Highcharts map with bubbles'
+      },
+
+      legend: {
+        enabled: false
+      },
+
+      mapNavigation: {
+        enabled: false
+        // enabled: true,
+        // buttonOptions: {
+        //   verticalAlign: 'bottom'
+        // }
+      },
+
+      series: [{
+        name: 'Countries',
+        color: '#E0E0E0',
+        enableMouseTracking: false
+      }, {
+        type: 'mapbubble',
+        name: 'Population 2013',
+        joinBy: ['iso-a2', 'code'],
+        data: [ { code: "AF", z: 30552 }, { code: "AL", z: 2897 } ],
+        minSize: 4,
+        maxSize: '12%',
+        tooltip: {
+          pointFormat: '{point.code}: {point.z} thousands'
+        }
+      }]
+    };
   };
 })();
