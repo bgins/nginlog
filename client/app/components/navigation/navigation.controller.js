5ba5e801795faa9798c8056c884cff30e17f1598
Brian Ginsburg <brian@brianginsburg.com>
2017-08-06 23:33:21 -0700

    Add navigation to sidenav
    Work on UI elements of sidenav.

--- client/app/components/navigation/navigation.controller.js
+++ client/app/components/navigation/navigation.controller.js
@@ -2,11 +2,17 @@
   'use strict';
 
   angular.module("nginlog.Controllers")
-    .controller('navigationController', navigationController);
+    .controller('navigationController', ["$scope", "$location", "$mdSidenav", navigationController]);
 
-  function navigationController($scope, $mdSidenav) {
+  function navigationController($scope, $location, $mdSidenav) {
     $scope.toggleSidenav = function() {
       $mdSidenav('left').toggle();
     };
+
+    $scope.navigate = function(url) {
+      $location.path(url);
+      $scope.toggleSidenav();
+    };
+
   };
 })();
