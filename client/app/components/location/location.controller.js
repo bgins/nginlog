b7cf8932bbcfda0ee92ef50f1c73cfe4cbfb49fb
Brian Ginsburg <brian@brianginsburg.com>
2017-08-15 20:06:01 -0700

    Work on data-table.

--- client/app/components/location/location.controller.js
+++ client/app/components/location/location.controller.js
@@ -7,10 +7,15 @@
   function locationController($scope, visualizationService) {
     visualizationService.fetch('total_requests_by_country_name','group=true')
       .then(function(result) {
-
         $scope.rows = result.rows;
-
+        $scope.rows.count = result.rows.length;
       });
+
+    $scope.query = {
+      order: 'key',
+      limit: 5,
+      page: 1
+    };
   };
 
 })();
