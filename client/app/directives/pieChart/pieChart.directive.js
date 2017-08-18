a56e6b1b8613e3df751dd0c79ae16b444dac84e1
Brian Ginsburg <brian@brianginsburg.com>
2017-08-18 11:41:38 -0700

    Add bar charts, one and two series.

--- client/app/directives/pieChart/pieChart.directive.js
+++ client/app/directives/pieChart/pieChart.directive.js
@@ -13,9 +13,6 @@
   };
 
   function pieChartController($scope, visualizationService) {
-    // test call to service
-    // data = visualizationService.fetch('total_requests_by_country_name','group=true');
-
     visualizationService.fetch('total_requests_by_country_name','group=true')
       .then(function(result) {
         var results = {};
@@ -49,13 +46,65 @@
         $scope.labels = labels;
         $scope.data = data;
         $scope.options = {legend:
-                          { display: true,
-                            position: 'bottom',
-                            labels: {
-                              // fontColor: 'white'
-                            }
+                          { display: false,
+                            position: 'bottom'
                           }
                          };
       });
   };
+
+
+// function pieChartController($scope, visualizationService) {
+//     visualizationService.fetch('country_name_ip_pairs','group=true')
+//       .then(function(result) {
+//         var results = {};
+//         var labels = [];
+//         var data = [];
+//         var totalData = [];
+//         var uniqueData = [];
+
+//         // transform result from [{key: ["country_name", numIp], value: "count"}, ...]
+//         // to [{"country_name": [numTotal, numUnique]}, ...]
+//         var label = "";
+//         var currLabel = "";
+//         for (var i = 0; i < result.rows.length; i += 1) {
+//           label = result.rows[i].key[0];
+//           if (label !== currLabel) {
+//             results[result.rows[i].key[0]] = [result.rows[i].value, 1];
+//             currLabel = label;
+//           } else {
+//             results[currLabel] = [results[currLabel][0] + result.rows[i].value, results[currLabel][1] + 1];
+//           }
+//         }
+
+//         // get keys sorted by value descending
+//         var keys = Object.keys(results).sort(function(a,b) { return results[b][0] - results[a][0]; });
+
+//         for (var j = 0; j < keys.length; j += 1) {
+//           if (j < 6) {
+//             labels.push(keys[j]);
+//             totalData.push(results[keys[j]][0]);
+//             uniqueData.push(results[keys[j]][1]);
+//           } else {
+//             if (j === 6) {
+//               labels.push('Other');
+//               totalData.push(0);
+//               uniqueData.push(0);
+//             }
+//             totalData[6] += results[keys[j]][0];
+//             uniqueData[6] += results[keys[j]][1];
+//           }
+//         }
+
+//         $scope.labels = labels;
+//         $scope.data = [totalData, uniqueData];
+//         $scope.series = ['Total', 'Unique'];
+//         $scope.options = {legend:
+//                           { display: true,
+//                             position: 'bottom'
+//                           }
+//                          };
+//       });
+//   };
+
 })();
