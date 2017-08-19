1604bc8bb56eb2d217cc18f93bdc9d6e7ba4eb17
Brian Ginsburg <brian@brianginsburg.com>
2017-08-18 22:29:05 -0700

    Add data-table and location charts
    All are directives now, with a responsive directive for the chart.
    The responsive part is not satisfactory.

--- client/app/components/location/location.controller.js
+++ client/app/components/location/location.controller.js
@@ -5,17 +5,7 @@
     .controller('locationController', ["$scope", "visualizationService", locationController]);
 
   function locationController($scope, visualizationService) {
-    visualizationService.fetch('total_requests_by_country_name','group=true')
-      .then(function(result) {
-        $scope.rows = result.rows;
-        $scope.rows.count = result.rows.length;
-      });
 
-    $scope.query = {
-      order: 'key',
-      limit: 5,
-      page: 1
-    };
   };
 
 })();
